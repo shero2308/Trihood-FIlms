bacbac97332d6521b50410f06a7242d44127d791
shero <rakshitkumar682@gmail.com>
2025-03-19 15:36:45 +0530

    feat: add motion effects to Publishing Platforms page; enhance layout with animated elements and hover effects

--- src/app/publishing-platforms/page.tsx
+++ src/app/publishing-platforms/page.tsx
@@ -11,26 +11,60 @@ import Facebook from "@/assets/facebook.png";
 import Instagram from "@/assets/instagram.png";
 
 import Image from "next/image";
+import * as motion from "motion/react-client"
 
 
 export default function Publishing_platform() {
+    const transition = {
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }
     return ( 
         <div>
             <Social_links />
             <Navbar />
             <div className="flex items-center flex-col pt-20">
+            <motion.div 
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{opacity:1, scale: 1 }}
+                    transition={{ duration: 0.8,
+                                  delay: 0.2,
+                                  ease: [0, 0.71, 0.2, 1.01]}}>
                 <Image src={Publish} alt="publishing" height={800} width={400} className="rounded-lg"/>
-                <h1 className="font-kanit text-4xl lg:text-5xl mt-14">
+            </motion.div>
+                <motion.h1
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{opacity:1, scale: 1 }}
+                    transition={{ duration: 0.8,
+                                  delay: 0.4,
+                                  ease: [0, 0.71, 0.2, 1.01]}}
+                    className="font-kanit text-4xl lg:text-5xl mt-14">
                     Publishing Platforms
-                </h1>
-                <p className="font-kanit italic text-xl lg:text-2xl px-10 text-center mt-3">
+                </motion.h1>
+                <motion.p
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{opacity:1, scale: 1 }}
+                    transition={{ duration: 0.8,
+                                  delay: 0.8,
+                                  ease: [0, 0.71, 0.2, 1.01]}} 
+                    className="font-kanit italic text-xl lg:text-2xl px-10 text-center mt-3">
                     Regional TV Channels, Movie Theater & Targeted Social Media Platform where we publish
-                </p>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4 md:px-10 lg:px-20 mt-10">
-                    <Image src={Youtube} alt="youtube" height={130} width={130} className="rounded-lg"/>
-                    <Image src={Facebook} alt="facebook" height={130} width={130} className="rounded-lg"/>
-                    <Image src={Instagram} alt="instagram" height={130} width={130} className="rounded-lg"/>
-                </div>
+                </motion.p>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{opacity:1, scale: 1 }}
+                    transition={{ duration: 0.8,
+                                  delay: 1.0,
+                                  ease: [0, 0.71, 0.2, 1.01]}} 
+                    className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4 md:px-10 lg:px-20 mt-10">
+                    <Image src={Youtube} alt="youtube" height={130} width={130} className="rounded-lg transition delay-150
+                     duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"/>
+                    <Image src={Facebook} alt="facebook" height={130} width={130} className="rounded-lg transition delay-150
+                     duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"/>
+                    <Image src={Instagram} alt="instagram" height={130} width={130} className="rounded-lg transition delay-150
+                     duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"/>
+                </motion.div>
             </div>
                 <div className="pt-40">
                   <Parallax />
